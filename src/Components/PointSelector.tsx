76ce0598407e894f38de595dd894aedaa0095f41
Dawid <qkari177@gmail.com>
2026-03-17 20:53:53 +0100

    Stepper

--- src/Components/PointSelector.tsx
+++ src/Components/PointSelector.tsx
@@ -4,6 +4,33 @@ import { Button } from "../ui/Button/Button";
 export const PointSelector = () => {
   const [han, setHan] = useState(1);
   const [fu, setFu] = useState(30);
+
+  const IncrementHan = () => {
+    if (han >= 27) {
+      return;
+    } else setHan((prev) => prev + 1);
+  };
+
+  const DecrementHan = () => {
+    if (han <= 1) {
+      return;
+    } else setHan((prev) => prev - 1);
+  };
+
+  const IncrementFu = () => {
+    if (fu >= 110) {
+      return;
+    } else if (fu < 30) {
+      setFu((prev) => prev + 5);
+    } else setFu((prev) => prev + 10);
+  };
+  const DecrementFu = () => {
+    if (fu <= 20) {
+      return;
+    } else if (fu <= 30) {
+      setFu((prev) => prev - 5);
+    } else setFu((prev) => prev - 10);
+  };
   return (
     <>
       <main className=" absolute top-0 left-0 w-full h-full flex items-center justify-center">
@@ -13,11 +40,13 @@ export const PointSelector = () => {
             <Button
               label={"-"}
               className={"p-2 bg-amber-500 border rounded-2xl"}
+              onClick={DecrementHan}
             />
             <h2>{han}</h2>
             <Button
-              label={"-"}
+              label={"+"}
               className={"p-2 bg-amber-500 border rounded-2xl"}
+              onClick={IncrementHan}
             />
           </div>
           <h1>Fu</h1>
@@ -25,16 +54,18 @@ export const PointSelector = () => {
             <Button
               label={"-"}
               className={"p-2 bg-amber-500 border rounded-2xl"}
+              onClick={DecrementFu}
             />
             <h2>{fu}</h2>
             <Button
-              label={"-"}
+              label={"+"}
               className={"p-2 bg-amber-500 border rounded-2xl"}
+              onClick={IncrementFu}
             />
           </div>
           <Button
             label={"ok"}
-            className={"p-2 bg-amber-500 border rounded-2xl"}
+            className={"p-2 bg-amber-500 border rounded-2xl px-10"}
           />
         </section>
       </main>
