dc7df4faedce87123e0aeccede9070cd8b9db48b
Dawid <qkari177@gmail.com>
2026-05-16 21:33:52 +0200

    Layout for choice of game

--- src/Pages/Hanchan/HanForm.tsx
+++ src/Pages/Hanchan/HanForm.tsx
@@ -2,8 +2,10 @@ import { Link, useNavigate } from "react-router";
 import { Button } from "../../ui/Button/Button";
 import { Input } from "../../ui/Button/Input";
 import { useHan } from "./HanContext";
+import { useEffect, useState } from "react";
 
 export const HanForm = () => {
+  const [isSave, setIsSave] = useState(false);
   const {
     playerList,
     setPlayerList,
@@ -13,16 +15,22 @@ export const HanForm = () => {
     commitRound,
   } = useHan();
   const handlePlayerName = (e, index) => {
-  const updatedPlayers = playerList.map((player, i) =>
-    i === index
-      ? { ...player, name: e.target.value }
-      : player
-  );
+    const updatedPlayers = playerList.map((player, i) =>
+      i === index ? { ...player, name: e.target.value } : player,
+    );
 
-  setPlayerList(updatedPlayers);
-};
+    setPlayerList(updatedPlayers);
+  };
+  useEffect(() => {
+    const storedPlayers = localStorage.getItem("players");
 
-  const navigate = useNavigate();
+    if (storedPlayers) {
+      setIsSave(true);
+    } else {
+      return;
+    }
+  }, []);
+
   return (
     <div className="bg-yellow-300 flex-col flex h-screen w-screen gap-5">
       <header className="h-10 bg-green-400 flex justify-center">
@@ -54,10 +62,25 @@ export const HanForm = () => {
             className={"bg-red-400"}
             label="Next"
             onClick={() => {
-                            commitPlayers(playerList);
+              commitPlayers(playerList);
               window.location.href = "/hanboard";
             }}
           />
+        </section>
+        <section
+          className={
+            isSave
+              ? `absolute top-0 left-0 flex w-full h-full justify-center items-center flex-col`
+              : "hidden"
+          }
+        >
+          <div className="bg-blue-400 w-4/5 h-1/3 rounded-2xl flex flex-col justify-around p-5 ">
+          <h1 className="text-white text-2xl">Resume ongoing game, or start new game?</h1>
+            <div className="flex justify-around gap-5">
+              <Button label={"Resume"} className={"bg-green-400 text-xl w-full py-2 rounded-xl text text-white"}></Button>{" "}
+              <Button label={"New Game"} className={"bg-red-400 text-xl w-full py-2 rounded-xl text text-white"}></Button>
+            </div>
+          </div>
         </section>
       </main>
     </div>
